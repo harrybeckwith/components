b3139e9442d69fdf1fb2746ec40193e6f934a3b5
hjb23 <harryjbeckwith@gmail.com>
2018-03-23 10:13:35 +0000

    first commit

---
+++ src/components/User.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+               <button @click ='changeName'>Change Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :myName ='name'></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function() {
+            return {
+                name: 'Harry'
+            }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        },
+        methods: {
+            changeName() {
+                this.name = 'Anna';
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{ switchName() }} </p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            myName: String,
+            default: 'Max'
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("");
+            },
+            resetName() {
+                this.myName = 'Harry';
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+
+</style>
